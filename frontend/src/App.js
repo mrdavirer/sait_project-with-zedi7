5e6192e8739a6375a58235e47e8f9fa8ead950f1
David <mrdavirework@gmail.com>
2025-02-07 21:17:40 +0300

    Single commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import MainPage from './components/MainPage'
 
 function App() {
-  return (
-    
-  );
+  <BrowserRouter>
+  <Header/>
+  <Routes>
+    <Route path="/" element={<MainPage/>}/>
+  </Routes>
+  <Footer/>
+</BrowserRouter>
 }
 
 export default App;
